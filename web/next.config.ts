516f4989557380049c950bb894c9ef37f3af2298
vietviet08 <vietnq.23ceb@vku.udn.vn>
2026-03-22 23:07:46 +0700

    fix(web): position sidebar toggle on border edge

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    // serverExternalPackages: ["pdfjs-dist"],
+    // webpack: (config) => {
+    //   config.resolve.alias.canvas = false;
+    //   config.resolve.alias.encoding = false;
+    //   return config;
+    // },
 };
 
 export default nextConfig;
